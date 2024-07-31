2442d45234a61fe425c5ea99c6e94e2514d99d8a
jgwjansen <61779664+jgwjansen@users.noreply.github.com>
2024-07-31 16:04:38 +0200

    initial

---
+++ src/main/java/com/example/garagenew/GarageNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.garagenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/garagenew/GarageNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garagenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
