8398a13066c9c61ba8474d754c854b598afffb21
jgwjansen <61779664+jgwjansen@users.noreply.github.com>
2024-07-31 17:06:44 +0200

    Klant aanmaken en bekijken opzet

--- src/main/java/com/example/garagenew/controller/controllers/KlantController.java
+++ src/main/java/com/example/garagenew/controller/controllers/KlantController.java
@@ -2,10 +2,13 @@ package com.example.garagenew.controller.controllers;
 
 import com.example.garagenew.domein.Klant;
 import com.example.garagenew.repository.KlantRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/klant")
@@ -17,13 +20,24 @@ public class KlantController {
         this.klantRepository = klantRepository;
     }
 
-//    @GetMapping
-//    //Haalt  klantgegevens op
-//    public String getKlant(){
-//
-//        return Garage2Application.ingelogdeGebruiker.getRollen().toString();
-//    }
+    @GetMapping
+    //Haalt  klantgegevens op
+    public List<Klant> getKlant(){
 
+        return klantRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    //Haalt  klantgegevens op
+    public ResponseEntity<?> getKlant(@PathVariable("id") Long id){
+        System.out.println(id);
+
+        Optional<Klant> klant = klantRepository.findById(id);
+        if(klant.isPresent()){
+            return ResponseEntity.ok(klant);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
     @PostMapping
     //Maakt nieuwe klant aan
@@ -31,7 +45,7 @@ public class KlantController {
         if(!Objects.equals(klant.getNaam(), "")){
             Klant newKlant = new Klant();
             newKlant.setNaam(klant.getNaam());
-            klantRepository.save(newKlant);
+            klantRepository.save(klant);
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.badRequest().build();
